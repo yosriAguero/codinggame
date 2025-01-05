55abfae2ba960fbf4b2d59f8f11dab3eb2ca970a
yosriAguero <yosri.jebri@gmail.com>
2025-01-05 22:28:13 +0100

    //test git

--- Csharp/SolutionCodingGame/CodingGame/Program.cs
+++ Csharp/SolutionCodingGame/CodingGame/Program.cs
@@ -18,6 +18,7 @@ using CodingGame.YEET_YONIK;
 StringCompression.M1();
 
 //ProductExcept.M1();
+//test git
 int[] tabtemperatures = { 73, 74, 75, 71, 69, 72, 76, 73 };
 DailyTemperatureYosri.Temperatures(tabtemperatures);
 DailyTemperatureYosri.DailyTemperatures(tabtemperatures);
